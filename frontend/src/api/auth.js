b8d0270f9144d7f59231c15dbe8fff55e1b3177e
Shashwat Dubey <i.shashwatdubey@gmail.com>
2025-08-07 17:44:28 +0530

    CORS Updation

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,27 +1,36 @@
 import axios from "./index";
 
 export const register = async (userData) => {
-
-  const response = await axios.post("/auth/signup", userData);
+  const response = await axios.post("/auth/signup", userData, {
+    withCredentials: true,
+  });
   return response.data;
 };
 
 export const login = async (credentials) => {
-  const response = await axios.post("/auth/login", credentials);
+  const response = await axios.post("/auth/login", credentials, {
+    withCredentials: true,
+  });
   return response.data;
 };
 
 export const logout = async () => {
-  const response = await axios.post("/auth/logout");
+  const response = await axios.post("/auth/logout", null, {
+    withCredentials: true,
+  });
   return response.data;
 };
 
 export const checkAuth = async () => {
-  const response = await axios.get("/auth/check");
+  const response = await axios.get("/auth/check", {
+    withCredentials: true,
+  });
   return response.data;
 };
 
 export const updateProfile = async (profileData) => {
-  const response = await axios.put("/auth/update-profile", profileData);
+  const response = await axios.put("/auth/update-profile", profileData, {
+    withCredentials: true,
+  });
   return response.data;
 };
